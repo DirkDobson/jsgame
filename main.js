e5bbac64ff04df1c377aa784d6511650f1880982
Dirk Dobson <dirkvancouver@live.com>
2018-08-23 17:26:17 -0600

    sudo code

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-/* 
-
-
-
+/* There is an array and each element of it represents postiions of play
+user click on one of the squares it puts a x on the position and removes it from the array
+ a random number is then generated to tell the function which avalible postiions of the array to put a circle
+There is then a compare function that compare if the user or computer has selected a possiblity to win
 */
-var table = [a,b,c,d,e,f,g]
+var table = [a,b,c,d,e,f,g,h,i]
 
 
 
