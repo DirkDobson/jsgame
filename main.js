ef3ec6af52c1f2cd25528ae9865f2446c5294e41
Dirk Dobson <dirkvancouver@live.com>
2018-08-23 20:06:43 -0600

    add function

--- main.js
+++ main.js
@@ -5,5 +5,31 @@ There is then a compare function that compare if the user or computer has select
 */
 var table = [a,b,c,d,e,f,g,h,i]
 
+var aa = document.getElementById('aa')
+var bb = document.getElementById('bb')
+var cc = document.getElementById('cc')
+var dd = document.getElementById('dd')
+var ee = document.getElementById('ee')
+var ff = document.getElementById('ff')
+var gg = document.getElementById('gg')
+var hh = document.getElementById('hh')
+var ii = document.getElementById('ii')
+
+
+aa.addEventListener('click',function(){
+check(aa,a)
+console.log(table)
+})
+
+function check(button, array,) {
+  table.forEach(function() {
+    if (button = array) 
+    table.delete[array]
+  })
+// when button is pressed remove the corrisponding array element
+}
+
+
+
 
 
